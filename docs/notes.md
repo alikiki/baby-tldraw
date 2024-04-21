e45d93b003ecb96ba88cf51b9b826341bb1781b0
Hye Woong Jeon <ajeon66@gmail.com>
2024-04-21 11:03:00 -0500

    expanded canvas to full size

---
+++ docs/notes.md
@@ -0,0 +1,5 @@
+Moneymaker for drawing shapes
+https://github.com/tldraw/tldraw/blob/main/packages/tldraw/src/lib/shapes/geo/components/SolidStylePolygon.tsx#L5
+
+SVG container that wraps around the shape
+https://github.com/tldraw/tldraw/blob/main/packages/editor/src/lib/components/SVGContainer.tsx#L8
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,9 +9,9 @@ interface Point {
 }
 
 export default function Canvas({ options }: BabyTLCanvasProps) {
-    const [camera, setCamera] = useState({ x: 0, y: 0, z: 1 });
-    const [initialCamera, setInitialCamera] = useState({ x: 0, y: 0, z: 1 });
-    const [startPos, setStartPos] = useState({ x: 0, y: 0 });
+    const [camera, setCamera] = useState<BabyTLCamera>({ x: 0, y: 0, z: 1 });
+    const [initialCamera, setInitialCamera] = useState<BabyTLCamera>({ x: 0, y: 0, z: 1 });
+    const [startPos, setStartPos] = useState<Point>({ x: 0, y: 0 });
 
     const [interactionEvents, setInteractionEvents] = useState({
         pointerDown: false,
@@ -20,70 +20,21 @@ export default function Canvas({ options }: BabyTLCanvasProps) {
 
     const rCanvas = useRef<HTMLDivElement>(null);
 
-    const viewportToGlobal = (point: Point, camera: BabyTLCamera): BabyTLCamera => {
+    const viewportToGlobal = (point: Point, camera: BabyTLCamera): Point => {
         return {
             x: (point.x / camera.z) - camera.x,
-            y: (point.y / camera.z) - camera.y,
-            z: camera.z
+            y: (point.y / camera.z) - camera.y
         }
     }
 
-    const handlePointerDown = (e: React.PointerEvent) => {
-        setStartPos({ x: e.clientX, y: e.clientY });
-        setInteractionEvents({ ...interactionEvents, pointerDown: true });
+    const globalToViewport = (point: Point, camera: BabyTLCamera): Point => {
+        return {
+            x: (point.x + camera.x) * camera.z,
+            y: (point.y + camera.y) * camera.z
+        }
     }
 
-    const handlePointerMove = (e: React.PointerEvent) => {
-        const { pointerDown } = interactionEvents;
-        if (!pointerDown) return;
-
-        const offsetX = startPos.x - e.clientX;
-        const offsetY = startPos.y - e.clientY;
-
-        const newCameraX = initialCamera.x - (offsetX / camera.z);
-        const newCameraY = initialCamera.y - (offsetY / camera.z);
-
-        setCamera({ x: newCameraX, y: newCameraY, z: camera.z });
-    }
-
-    const handlePointerUp = (e: React.PointerEvent) => {
-        setInteractionEvents({ ...interactionEvents, pointerDown: false });
-        setInitialCamera({ ...camera });
-    }
-
-    // const handleKeyDown = (e: React.KeyboardEvent) => {
-    //     if (e.metaKey) {
-    //         editorDispatch({ object: "keyboard", command: { key: "meta", direction: "down" } });
-    //     }
-    // };
-
-    // const handleKeyUp = (e: React.KeyboardEvent) => {
-    //     if (!e.metaKey) {
-    //         editorDispatch({ object: "keyboard", command: { key: "meta", direction: "up" } });
-    //     }
-    // };
-
-    // BUG:
-    // basically the problem is that the zooming is not happening around the pointer
-    // every time I zoom, the position of my pointer in global space changes very slightly
-
-    // the pointer in global space should be invariant.
-
-    // like concenptually i dont understand how the zoom works.
-
-    const pointerHandlers = {
-        onPointerDown: handlePointerDown,
-        onPointerMove: handlePointerMove,
-        onPointerUp: handlePointerUp
-    };
-
-
-    // const keyHandlers = {
-    //     onKeyDown: handleKeyDown,
-    //     onKeyUp: handleKeyUp
-    // }
-
-    const zoomCamera = (point: Point, camera: BabyTLCamera, zoomOffset: number) => {
+    const zoomCamera = (point: Point, camera: BabyTLCamera, zoomOffset: number): BabyTLCamera => {
         const zoom = camera.z - zoomOffset * camera.z;
 
         const p1 = viewportToGlobal(point, camera);
@@ -96,17 +47,52 @@ export default function Canvas({ options }: BabyTLCanvasProps) {
         };
     }
 
+    const handlePointerDown = (e: React.PointerEvent) => {
+        console.log("pointer down");
+        setInitialCamera(camera);
+        setStartPos({ x: e.clientX, y: e.clientY });
+        setInteractionEvents({ ...interactionEvents, pointerDown: true });
+    }
+
+    const handlePointerMove = (e: React.PointerEvent) => {
+        const { pointerDown } = interactionEvents;
+        if (!pointerDown) return;
+
+        console.log(e);
+
+        const offsetX = startPos.x - e.clientX;
+        const offsetY = startPos.y - e.clientY;
+
+        const newCameraX = initialCamera.x - (offsetX / camera.z);
+        const newCameraY = initialCamera.y - (offsetY / camera.z);
+
+        setCamera({ x: newCameraX, y: newCameraY, z: camera.z });
+    }
+
+    const handlePointerUp = (e: React.PointerEvent) => {
+        setInteractionEvents({ ...interactionEvents, pointerDown: false });
+        setInitialCamera(camera);
+    }
+
+    const pointerHandlers = {
+        onPointerDown: handlePointerDown,
+        onPointerMove: handlePointerMove,
+        onPointerUp: handlePointerUp
+    };
+
+
+
     useEffect(() => {
         const handleZoom = (e: React.WheelEvent) => {
             e.preventDefault();
             console.log(e);
-            const { offsetX: x, offsetY: y, deltaY, metaKey } = e;
+            const { clientX: x, clientY: y, deltaY, metaKey } = e;
 
             if (!metaKey) return;
 
-            console.log('client coords:', x, y);
-            console.log('----');
-
+            // IMPORTANT:
+            // Using a functional update here to ensure that the camera updates
+            // are properly ordered.
             setCamera((camera) => zoomCamera({ x: x, y: y }, camera, deltaY * 0.01));
         }
 
