376824e23352f2b16cb28ad27539e0da93cd5872
Hye Woong Jeon <ajeon66@gmail.com>
2024-04-21 09:36:23 -0500

    BUG: weird arc to zooming behavior. zooming still doesn't work even with using offsetX, offsetY. have to look into later.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -7,6 +7,7 @@ import { useEditorDispatch } from "../hooks/useEditorDispatch";
 export default function Canvas({ options }: BabyTLCanvasProps) {
     const editor = useEditor();
     const editorDispatch = useEditorDispatch();
+
     const [startPos, setStartPos] = useState({ x: 0, y: 0 });
 
     const rCanvas = useRef<HTMLDivElement>(null);
@@ -46,52 +47,18 @@ export default function Canvas({ options }: BabyTLCanvasProps) {
     }
 
     const handleKeyDown = (e: React.KeyboardEvent) => {
+        console.log('key down', e.key, e.metaKey);
         if (e.metaKey) {
             editorDispatch({ object: "keyboard", command: { key: "meta", direction: "down" } });
         }
     };
 
     const handleKeyUp = (e: React.KeyboardEvent) => {
+        console.log('key up', e.key, e.metaKey);
         if (!e.metaKey) {
             editorDispatch({ object: "keyboard", command: { key: "meta", direction: "up" } });
         }
     };
-
-    const handleZoom = (e: React.WheelEvent) => {
-        if (!editor.isMetaDown) return;
-        console.log('zooming', e.deltaY);
-        const zoomOffset = (e.deltaY * 0.005);
-        const newZoom = editor.camera.z + zoomOffset;
-        const cappedZoom = Math.min(4, Math.max(0.1, newZoom));
-
-        console.log('client pointer position', e.clientX, e.clientY);
-        console.log('camera position', editor.camera.x, editor.camera.y);
-
-        const offsetX = ((e.clientX / cappedZoom) - editor.camera.x) - editor.camera.x;
-        const offsetY = ((e.clientY / cappedZoom) - editor.camera.y) - editor.camera.y;
-
-        console.log('offsetX', offsetX);
-        console.log('offsetY', offsetY);
-
-        const newCameraX = editor.camera.x + (offsetX * zoomOffset);
-        const newCameraY = editor.camera.y + (offsetY * zoomOffset);
-
-
-        editorDispatch({
-            object: "camera", command: {
-                x: newCameraX,
-                y: newCameraY,
-                z: cappedZoom,
-            }
-        })
-        editorDispatch({
-            object: "initial-camera", command: {
-                x: newCameraX,
-                y: newCameraY,
-                z: cappedZoom,
-            }
-        })
-    }
 
     const pointerHandlers = {
         onPointerDown: handlePointerDown,
@@ -99,27 +66,60 @@ export default function Canvas({ options }: BabyTLCanvasProps) {
         onPointerUp: handlePointerUp
     };
 
-    const wheelHandlers = {
-        onWheel: handleZoom
-    }
 
     const keyHandlers = {
         onKeyDown: handleKeyDown,
         onKeyUp: handleKeyUp
     }
 
+
     useEffect(() => {
+        const handleZoom = (e: React.WheelEvent) => {
+            if (!editor.isMetaDown) return;
+            const { clientX: x, clientY: y, deltaY } = e;
+
+            const zoomOffset = (deltaY * 0.005);
+            const newZoom = editor.camera.z + zoomOffset;
+            const cappedZoom = Math.min(4, Math.max(0.1, newZoom));
+
+            console.log('client pointer position', x, y);
+            console.log('camera position', editor.camera.x, editor.camera.y);
+
+            const offsetX = ((x / cappedZoom) - editor.camera.x) - editor.camera.x;
+            const offsetY = ((y / cappedZoom) - editor.camera.y) - editor.camera.y;
+
+            console.log('offsetX', offsetX);
+            console.log('offsetY', offsetY);
+
+            const newCameraX = editor.camera.x + (offsetX * zoomOffset);
+            const newCameraY = editor.camera.y + (offsetY * zoomOffset);
+
+            editorDispatch({
+                object: "camera", command: {
+                    x: newCameraX,
+                    y: newCameraY,
+                    z: cappedZoom,
+                }
+            })
+            editorDispatch({
+                object: "initial-camera", command: {
+                    x: newCameraX,
+                    y: newCameraY,
+                    z: cappedZoom,
+                }
+            })
+        }
+
         if (!rCanvas.current) return;
-        rCanvas.current.focus();
         rCanvas.current.addEventListener('wheel', handleZoom, { passive: false });
 
         return () => {
             rCanvas.current?.removeEventListener('wheel', handleZoom);
         }
-    }, [rCanvas.current])
+    }, [rCanvas, editor])
 
     return (
-        <div ref={rCanvas} style={{ width: options.width, height: options.height, border: "1px solid black" }} {...pointerHandlers} {...wheelHandlers} {...keyHandlers} tabIndex={0} draggable={false}>
+        <div className="canvas-wrapper" ref={rCanvas} {...pointerHandlers} {...keyHandlers} tabIndex={0} draggable={false}>
             <InnerCanvas />
         </div>
     )
--- src/components/InnerCanvas.tsx
+++ src/components/InnerCanvas.tsx
@@ -4,21 +4,7 @@ export default function InnerCanvas() {
     const editor = useEditor();
 
     return (
-        <div style={{ transform: `scale(${editor.camera.z}) translate(${editor.camera.x}px, ${editor.camera.y}px)` }}>
-            {Array.from({ length: 10 }, (_, row) => {
-                return Array.from({ length: 10 }, (_, i) => {
-                    return (< div key={i}
-                        style={{
-                            position: "absolute",
-                            top: (50 * row),
-                            left: (50 * i),
-                            width: 2,
-                            height: 2,
-                            backgroundColor: "red" // Changed to red
-                        }}>
-                    </div>)
-                })
-            })}
+        <div style={{ position: "relative", width: "800px", height: "600px", border: "1px solid blue", transform: `scale(${editor.camera.z}) translate(${editor.camera.x}px, ${editor.camera.y}px)` }}>
 
             {/* <div style={{ position: "absolute", top: 0, left: 0, width: 100, height: 100, backgroundColor: "red" }}></div>
             <div style={{ position: "absolute", top: 0, left: 105, width: 100, height: 100, backgroundColor: "red" }}></div> */}
